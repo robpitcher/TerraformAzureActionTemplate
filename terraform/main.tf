8a37f15750e91426526c657ce3409b7fc18a52db
robpitcher <13648061+robpitcher@users.noreply.github.com>
2024-09-27 13:11:00 -0400

    fix: linter error

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "baseRG" {
   name     = var.projectName
   location = "East US"
 }
+
+resource "azurerm_management_lock" "resource-group-level" {
+  name       = "resource-group-level"
+  scope      = azurerm_resource_group.example.id
+  lock_level = "ReadOnly"
+  notes      = "This Resource Group is Read-Only"
+}
